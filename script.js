700a3affecbed1f0c51ba0bf551619c2e4474119
matej-veselovsky <matej.veselovsky97@gmail.com>
2022-06-25 22:13:52 +0200

    Add game(), playRound() and evalRound()

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let BESTOF = 5;
+
 //generate random computer choice
 function computerSelection () {
     let randomNumber = Math.floor(Math.random()*3);
@@ -28,16 +30,64 @@ function playerSelection() {
 
     //return the input if it's one of three options
     if ((playerInputLower == "rock") || (playerInputLower == "paper") || (playerInputLower == "scissors")) return playerInputLower;
+    //propmpt player again if it doesn't match options
     else {
         console.log("Please pick either rock, paper or scissors.")
         return playerSelection();
     }
 
-    //propmpt player again if it doesn't match options
 }
 
 //evaluate matchup - playRound function
+function playRound() {
+    let computerInput = computerSelection();
+    let playerInput = playerSelection();
+    let roundWinner = evalRound(computerInput, playerInput);
+    
+    return roundWinner;
+}
 
 //run 5 rounds of the game
+function game() {
+    let playerPoints = 0;
+    for (let i = 0; i < BESTOF; i++) {
+        let isDraw = true;
+        let roundResult;
+        while (isDraw) {
+            roundResult = playRound();
+            if (roundResult !== "draw") {
+                isDraw = false;
+                if (roundResult === "player") {
+                    playerPoints++;
+                };
+            };
+        }
+    }
+
+    if (playerPoints > BESTOF/2) {
+        console.log("You have won the game!");
+    } else {
+        console.log("You have lost the game!")
+    }
+
+}
 
 //evaluate and announce the winner
+function evalRound(computerInput, playerInput) {
+    if (computerInput === playerInput) {
+        console.log(`You both pick ${playerInput}! It's a draw!`);
+        return "draw";
+    }
+
+    if (((computerInput === "rock") && (playerInput === "scissors"))
+    || ((computerInput === "scissors") && (playerInput === "paper"))
+    || ((computerInput === "paper") && (playerInput === "rock"))) {
+        console.log(`${computerInput} beats ${playerInput}!`);
+        console.log("You have lost this round!");
+        return "computer";
+    } else {
+        console.log(`${playerInput} beats ${computerInput}!`);
+        console.log("You have won this round!");
+        return "player";
+    }
+}
